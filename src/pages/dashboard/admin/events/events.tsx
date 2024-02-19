b6ef4b3ae8a6969f361ef4dc2a662241de42221e
hakam309 <happysngh309@yahoo.com>
2024-02-19 21:04:52 +0530

    menu-links-update-commit
    menu-links-update-commit

--- src/pages/dashboard/admin/events/events.tsx
+++ src/pages/dashboard/admin/events/events.tsx
@@ -446,8 +446,17 @@ const Events = () => {
       
       </div>
     </section>
-  </div>
+  </div>  {/*    <aside className="control-sidebar control-sidebar-dark">
+      </aside>
 
+      <footer className="main-footer">
+        <strong>Copyright &copy; 2024 .</strong>
+        All rights reserved.
+        <div className="float-right d-none d-sm-inline-block">
+          <b>Version</b> 3.1.0-pre
+        </div>
+      </footer>
+   */}
   </div>
   </div>
 );
--- src/pages/dashboard/admin/leftNav.tsx
+++ src/pages/dashboard/admin/leftNav.tsx
@@ -41,20 +41,25 @@ const leftNav = () => {
                     <a href="/dashboard" className="nav-link">Home</a>
                 </li>
                 <li className="nav-item d-none d-sm-inline-block">
+                    <a href="/dashboard" className="nav-link">Schedule</a>
+                </li>
+                <li className="nav-item d-none d-sm-inline-block">
+                    <a href="/dashboard" className="nav-link">FAQ</a>
+                </li>
+                <li className="nav-item d-none d-sm-inline-block">
+                    <a href="/dashboard" className="nav-link">Shop</a>
+                </li>
+                <li className="nav-item d-none d-sm-inline-block">
+                    <a href="/dashboard" className="nav-link">Partners</a>
+                </li>
+                <li className="nav-item d-none d-sm-inline-block">
+                    <a href="/dashboard" className="nav-link">About</a>
+                </li>
+                <li className="nav-item d-none d-sm-inline-block">
                     <a href="#" className="nav-link">Contact</a>
                 </li>
                 </ul>
 
-                <form className="form-inline ml-3">
-                <div className="input-group input-group-sm">
-                    <input className="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search" />
-                    <div className="input-group-append">
-                    <button className="btn btn-navbar" type="submit">
-                        <i className="fas fa-search"></i>
-                    </button>
-                    </div>
-                </div>
-                </form>
 
                 <ul className="navbar-nav ml-auto">
                 
